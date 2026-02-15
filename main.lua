f0dfa3d3d77c9ce954990eb950d0040c09311320
TheRobloxStudioCat <matvey4567d@gmail.com>
2026-02-15 21:35:50 +0800

    Created sample project.

---
+++ main.lua
@@ -0,0 +1,53 @@
+local font_size = 18
+
+local current_console = {"ScratchCode 2.0 example project", "Made by TheDreamingCat"}
+
+local scr_code = require("libraries.scratchcode.main")
+
+love.window.close()
+
+local function split(input)
+    local result = {}
+
+    local current = ""
+
+    local in_quotes = false
+    local quote_char = nil
+
+    for i = 1, #input do
+        local char = input:sub(i, i)
+
+        if (char == '"' or char == "'") then
+            if not in_quotes then
+                in_quotes = true
+
+                quote_char = char
+            elseif char == quote_char then
+                in_quotes = false
+
+                quote_char = nil
+            end
+            current = current .. char
+        elseif char == "&" and not in_quotes then
+            table.insert(result, current:match("^%s*(.-)%s*$"))
+
+            current = ""
+        else
+            current = current .. char
+        end
+    end
+
+    if current ~= "" then
+        table.insert(result, current:match("^%s*(.-)%s*$"))
+    end
+
+    return result
+end
+
+function love.update()
+    io.write("> ")
+
+    local run_to = split(io.read())
+
+    scr_code.comp_and_run(run_to)
+end
